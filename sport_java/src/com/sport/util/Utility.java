bcd65161b03a9854767a124e39c9c6e8f0a8f81f
santoshgowd <santoshbiradar284@gmail.com>
2023-02-19 14:34:29 +0530

    Utility updated from TL

--- sport_java/src/com/sport/util/Utility.java
+++ sport_java/src/com/sport/util/Utility.java
@@ -1,9 +1,13 @@
 package com.sport.util;
 
+import java.util.Date;
+
 public class Utility {
 
 	public static void main(String[] args) {
 		System.out.println("This utility is from TL");
+		Date d = new Date();
+		System.out.println(d.getDate());
 	}
 
 }
